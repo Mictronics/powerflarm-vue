8d72c8a2763b6443d2840da6d8ed188edeefdf8d
Mictronics <github@mictronics.de>
2026-03-08 20:33:01 +0100

    Added FLARM FLAU packet.

---
+++ packages/nmea-web-serial/packages/nmea-web-serial/src/parser/codecs/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Custom NMEA sentence codecs.
+ */
+
+export type { DBKPacket } from './DBK'
+export type { DBSPacket } from './DBS'
+export type { DPTPacket } from './DPT'
+export type { FLAUPacket } from './FLAU'
