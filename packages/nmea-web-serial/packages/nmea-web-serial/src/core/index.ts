e80e0421ca3063f9345cacafee425ae8524a6562
Mictronics <github@mictronics.de>
2026-03-10 19:33:15 +0100

    Removed nmea-web-serial. Not a sub-module.

--- packages/nmea-web-serial/packages/nmea-web-serial/src/core/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Core NMEA machine functionality.
- */
-
-export { NmeaClient } from './client'
-export type { NmeaClientOptions } from './client'
-export { createNmeaMachine, type NmeaMachineActor } from './machine'
-export type { NmeaContext, NmeaEvent, NmeaMachineConfig } from './types'
