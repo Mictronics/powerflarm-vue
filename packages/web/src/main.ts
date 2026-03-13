24c707510089c1f8c0ed8bc588657de1ed5ab2b0
Mictronics <github@mictronics.de>
2026-03-13 20:09:43 +0100

    Added virtual scroller.

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,36 +1,34 @@
-import './assets/main.css'
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from './App.vue'
+import App from './App.vue';
 import PrimeVue from 'primevue/config';
 import Lara from '@primeuix/themes/lara';
 
 import Button from 'primevue/button';
 import Card from 'primevue/card';
+import VirtualScroller from 'primevue/virtualscroller';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia());
 app.use(PrimeVue, {
-    theme: {
-        preset: Lara,
-        options: {
-            darkModeSelector: '.dark',
-            cssLayer: {
-                name: 'primevue',
-                order: 'theme, base, primevue, utilities'
-            }
-        }
-    }
+  theme: {
+    preset: Lara,
+    options: {
+      darkModeSelector: '.dark',
+      cssLayer: {
+        name: 'primevue',
+        order: 'theme, base, primevue, utilities',
+      },
+    },
+  },
 });
 
-[
-    Button,
-    Card,
-].forEach(component => {
-    app.component(component.name, component);
+[Button, Card, VirtualScroller].forEach((component) => {
+  app.component(component.name, component);
 });
 
-app.mount('#app')
+app.mount('#app');
